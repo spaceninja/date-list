8de07756581c19ecf75b4da99557b878c2b613b9
Scott Vandehey <(none)>
2017-08-24 08:56:09 -0700

    Use Let and Const
    This commit refactors vars to use ES6-style let and const variables.

--- js/listdates.js
+++ js/listdates.js
@@ -1,22 +1,23 @@
 /* global moment, humanizeDuration */
-var xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 
 // Convert date array into HTML
-var listDates = function(dates) {
-    var content = "";
-    for (var i = 0; i < dates.length; i++) {
-        var durationText = "";
-        var eventDate = moment(dates[i].date);
-        var diff = eventDate.diff(moment(), 'milliseconds');
-        var millisecondsMonth = moment.duration(1, 'months');
+const listDates = function(dates) {
+    let content = "";
+
+    for (let i = 0; i < dates.length; i++) {
+        const eventDate = moment(dates[i].date);
+        const diff = eventDate.diff(moment(), 'milliseconds');
+        const millisecondsMonth = moment.duration(1, 'months');
+        let durationText = "";
 
         // http://momentjs.com/docs/#/displaying/fromnow/
         // returns '25 days ago' or '19 hours ago'
-        var shortDuration = eventDate.fromNow();
+        let shortDuration = eventDate.fromNow();
 
         // https://github.com/EvanHahn/HumanizeDuration.js
         // returns '31 years, 8 months'
-        var longDuration = humanizeDuration(diff, { largest: 2, round: true });
+        let longDuration = humanizeDuration(diff, { largest: 2, round: true });
 
         // add human-readable duration text
         if (diff < 0) {
@@ -55,19 +56,19 @@ xhr.onload = function() {
 
     // We got a good response from the server
     if (xhr.status >= 200 && xhr.status < 400) {
-        var dates = JSON.parse(xhr.responseText);
+        const dates = JSON.parse(xhr.responseText);
         document.getElementById('datelist-content').innerHTML = listDates(dates);
 
     // We reached our target server, but it returned an error
     } else {
-        var error = 'Whoops! Something went wrong. Please try again.';
+        const error = 'Whoops! Something went wrong. Please try again.';
         document.getElementById('datelist-error').innerHTML = error;
     }
 };
 
 // Handle an Ajax connection error
 xhr.onerror = function() {
-    var error = 'Whoops! We couldn\'t reach the server. Please try again.';
+    const error = 'Whoops! We couldn\'t reach the server. Please try again.';
     document.getElementById('datelist-error').innerHTML = error;
 };
 
