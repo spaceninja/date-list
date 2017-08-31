6b912fbdd92d0cde002de0a052d48f77778c66cd
Scott Vandehey <fake-scott@spaceninja.com>
2017-08-31 16:04:21 -0700

    Use Let and Const (#15)
    * Use Let and Const
    
    This commit refactors vars to use ES6-style let and const variables.
    
    * strict mode

--- js/listdates-min.js
+++ js/listdates-min.js
@@ -1,1 +1,81 @@
-var xhr=new XMLHttpRequest,listDates=function(e){for(var t="",n=0;n<e.length;n++){var r="",a=moment(e[n].date),o=a.diff(moment(),"milliseconds"),s=moment.duration(1,"months"),l=a.fromNow(),d=humanizeDuration(o,{largest:2,round:!0});o<0?d+=" ago":d="in "+d,r=Math.abs(o)<s?l:d,t+="<tr><td>"+e[n].event+"</td><td class='num'>"+a.calendar(null,{sameElse:"ll"})+"</td><td class='num'>"+r+"</td></tr>"}return t};xhr.open("GET","data/dates.json",!0),xhr.onload=function(){if(xhr.status>=200&&xhr.status<400){var e=JSON.parse(xhr.responseText);document.getElementById("datelist-content").innerHTML=listDates(e)}else{var t="Whoops! Something went wrong. Please try again.";document.getElementById("datelist-error").innerHTML="Whoops! Something went wrong. Please try again."}},xhr.onerror=function(){var e="Whoops! We couldn't reach the server. Please try again.";document.getElementById("datelist-error").innerHTML="Whoops! We couldn't reach the server. Please try again."},xhr.send();
+/* global moment, humanizeDuration */
+const xhr = new XMLHttpRequest();
+
+// Convert date array into HTML
+const listDates = function(dates) {
+    'use strict';
+    let content = "";
+
+    for (let i = 0; i < dates.length; i++) {
+        const eventDate = moment(dates[i].date);
+        const diff = eventDate.diff(moment(), 'milliseconds');
+        const millisecondsMonth = moment.duration(1, 'months');
+        let durationText = "";
+
+        // http://momentjs.com/docs/#/displaying/fromnow/
+        // returns '25 days ago' or '19 hours ago'
+        const shortDuration = eventDate.fromNow();
+
+        // https://github.com/EvanHahn/HumanizeDuration.js
+        // returns '31 years, 8 months'
+        let longDuration = humanizeDuration(diff, { largest: 2, round: true });
+
+        // add human-readable duration text
+        if (diff < 0) {
+            // dates in the past
+            longDuration = longDuration + " ago";
+        } else {
+            // dates in the future
+            longDuration = "in " + longDuration;
+        }
+
+        // decide which text to use based on how long the duration is
+        if (Math.abs(diff) < millisecondsMonth) {
+            // durations shorter than a month
+            durationText = shortDuration;
+        } else {
+            // durations longer than a month
+            durationText = longDuration;
+        }
+
+        // construct the HTML table row
+        content += "<tr>" +
+                   "<td>" + dates[i].event + "</td>" +
+                   "<td class='num'>" + eventDate.calendar(null, {sameElse: 'll'}) + "</td>" +
+                   "<td class='num'>" + durationText + "</td>" +
+                   "</tr>";
+
+    }
+    return content;
+};
+
+// Prepare the Ajax request
+xhr.open('GET', 'data/dates.json', true);
+
+// Handle the Ajax response
+xhr.onload = function() {
+    'use strict';
+
+    // We got a good response from the server
+    if (xhr.status >= 200 && xhr.status < 400) {
+        const dates = JSON.parse(xhr.responseText);
+        document.getElementById('datelist-content').innerHTML = listDates(dates);
+
+    // We reached our target server, but it returned an error
+    } else {
+        const error = 'Whoops! Something went wrong. Please try again.';
+        document.getElementById('datelist-error').innerHTML = error;
+    }
+};
+
+// Handle an Ajax connection error
+xhr.onerror = function() {
+    'use strict';
+    const error = 'Whoops! We couldn\'t reach the server. Please try again.';
+    document.getElementById('datelist-error').innerHTML = error;
+};
+
+// Send the Ajax request
+xhr.send();
+
+
--- js/listdates.js
+++ js/listdates.js
@@ -1,22 +1,24 @@
 /* global moment, humanizeDuration */
-var xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 
 // Convert date array into HTML
-var listDates = function(dates) {
-    var content = "";
-    for (var i = 0; i < dates.length; i++) {
-        var durationText = "";
-        var eventDate = moment(dates[i].date);
-        var diff = eventDate.diff(moment(), 'milliseconds');
-        var millisecondsMonth = moment.duration(1, 'months');
+const listDates = function(dates) {
+    'use strict';
+    let content = "";
+
+    for (let i = 0; i < dates.length; i++) {
+        const eventDate = moment(dates[i].date);
+        const diff = eventDate.diff(moment(), 'milliseconds');
+        const millisecondsMonth = moment.duration(1, 'months');
+        let durationText = "";
 
         // http://momentjs.com/docs/#/displaying/fromnow/
         // returns '25 days ago' or '19 hours ago'
-        var shortDuration = eventDate.fromNow();
+        const shortDuration = eventDate.fromNow();
 
         // https://github.com/EvanHahn/HumanizeDuration.js
         // returns '31 years, 8 months'
-        var longDuration = humanizeDuration(diff, { largest: 2, round: true });
+        let longDuration = humanizeDuration(diff, { largest: 2, round: true });
 
         // add human-readable duration text
         if (diff < 0) {
@@ -52,22 +54,24 @@ xhr.open('GET', 'data/dates.json', true);
 
 // Handle the Ajax response
 xhr.onload = function() {
+    'use strict';
 
     // We got a good response from the server
     if (xhr.status >= 200 && xhr.status < 400) {
-        var dates = JSON.parse(xhr.responseText);
+        const dates = JSON.parse(xhr.responseText);
         document.getElementById('datelist-content').innerHTML = listDates(dates);
 
     // We reached our target server, but it returned an error
     } else {
-        var error = 'Whoops! Something went wrong. Please try again.';
+        const error = 'Whoops! Something went wrong. Please try again.';
         document.getElementById('datelist-error').innerHTML = error;
     }
 };
 
 // Handle an Ajax connection error
 xhr.onerror = function() {
-    var error = 'Whoops! We couldn\'t reach the server. Please try again.';
+    'use strict';
+    const error = 'Whoops! We couldn\'t reach the server. Please try again.';
     document.getElementById('datelist-error').innerHTML = error;
 };
 
