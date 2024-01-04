d93974d6a8a563d110fc6bbba01e77e122ba0342
Scott Vandehey <fake-scott@spaceninja.com>
2024-01-04 13:53:06 -0800

    bump stylelint, remove stylelint-config-prettier

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
-  extends: [
-    'stylelint-config-spaceninja',
-    'stylelint-config-recommended-vue',
-    'stylelint-config-prettier',
-  ],
+  extends: ['stylelint-config-spaceninja', 'stylelint-config-recommended-vue'],
   rules: {
     // override/add rules settings here, such as:
     // 'color-no-invalid-hex': false,
